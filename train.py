0cf563f143bcfdabe4b9b9e71352fd4102adfe73
gqkd <heartofreedom@gmail.com>
2022-07-18 07:18:37 +0200

    train with weigths

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ from model_cnn import net
 import tensorflow as tf
 import datetime
 import tensorflow_addons as tfa
-
+import numpy as np
 
 def trainer(X_train, X_valid, y_train, y_valid, 
             epochs=10, 
@@ -10,10 +10,27 @@ def trainer(X_train, X_valid, y_train, y_valid,
             log_dir="/logs/base_fit/", 
             model = None,
             lr = 1e-4,
-            patiente=100):
+            patiente=100,
+            weights=False,
+            ):
 
     num_classes = len(y_train[0])
     
+    if weights:
+        y_train_ = y_train.argmax(axis=1)
+        _ , counts = np.unique(y_train_, return_counts=True)
+        # under_represented_class = counts.argmin() #the under represented class
+        train_weights = y_train_
+        #creation of balanced vector for weights
+        #divide the max of the counts by every counts to have the "ratio"
+        ratio = max(counts)/counts
+        #now divide it by the sum to have a vector that sum up to 1
+        weights_ = ratio/sum(ratio)
+        sum(weights_) #must be 1
+        for i in range(len(weights_)):
+            train_weights = np.where(train_weights==i, weights_[i], train_weights)
+
+
     if model == None: #if not specified is the first training
         model = net()
     opt = tf.keras.optimizers.Adam(learning_rate=lr)
@@ -52,14 +69,23 @@ def trainer(X_train, X_valid, y_train, y_valid,
                                               verbose=1,
                                               save_best_only=True,
                                               save_weights_only=False)
-
-    history = model.fit(X_train, y_train,
-                epochs=epochs,
-                batch_size=batch,
-                verbose=1,
-                validation_data=(X_valid,y_valid),
-                callbacks=[tensorboard,early_stopping, checkpoint])
-    
+    if weights:
+      print("inside if of weights")
+      history = model.fit(X_train, y_train,
+                  epochs=epochs,
+                  batch_size=batch,
+                  verbose=1,
+                  sample_weight = train_weights, 
+                  validation_data=(X_valid,y_valid),
+                  callbacks=[tensorboard,early_stopping, checkpoint])
+    else:
+        history = model.fit(X_train, y_train,
+                    epochs=epochs,
+                    batch_size=batch,
+                    verbose=1,
+                    validation_data=(X_valid,y_valid),
+                    callbacks=[tensorboard,early_stopping, checkpoint])
+        
 
     # model.save( log_dir +'models/'+ datetime.datetime.now().strftime("%Y_%m_%d-%H%M"))
 
