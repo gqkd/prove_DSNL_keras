11866641d8ee44d02b54a32944d5900ae07e2ee8
gqkd <heartofreedom@gmail.com>
2022-07-25 14:12:30 +0200

    bug

--- download.py
+++ download.py
@@ -2,6 +2,7 @@
 import os
 
 os.system("cd download_dataset/data")
+os.system("pwd")
 os.system("chmod +x download_physionet.sh")
 os.system("./download_physionet.sh")
 os.system("python prepare_physionet_original.py --data_dir data --output_dir data/eeg_fpz_cz --select_ch 'EEG Fpz-Cz'")
